324feca73e0cb2b0324abcae2fc637c02bbb32bd
Jan Peter <jan.peter92@mail.de>
2022-03-18 15:43:30 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from "react";
-import Button from "../src/components/Button";
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+import "./index.css";
+
+const FEATURED_API =
+	"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4d7acc0cb3eb815dee451805a333e105&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+	"https://api.themoviedb.org/3/search/movie?&api_key=4d7acc0cb3eb815dee451805a333e105&query=";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello test</h1>
-      <Button>Click me!</Button>
-    </div>
-  );
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		fetch(FEATURED_API)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				setMovies(data.results);
+			});
+	}, []);
+
+	return (
+		<div>
+			<h2>Your Top Ten</h2>
+			<div className="movie-container">
+				{movies.length > 0 && movies.map(movie => <Movie key={movie.id} {...movie} />)}
+			</div>
+		</div>
+	);
 };
 
 export default App;
+
+// https://api.themoviedb.org/3/movie/550?api_key=4d7acc0cb3eb815dee451805a333e105
+// 4d7acc0cb3eb815dee451805a333e105
